0f28605ca98f07c177878b87965e94fdd1bb3f97
OTZro <a2584310@gmail.com>
2017-04-18 21:43:02 +0800

    [CHANGE] Fix build env script

--- bin/build_env.sh
+++ bin/build_env.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-SCRIPTPATH=$(dirname $0)
+absPath(){
+    if [[ -d "$1" ]]; then
+        cd "$1"
+        echo "$(pwd -P)"
+    else 
+        cd "$(dirname "$1")"
+        echo "$(pwd -P)/$(basename "$1")"
+    fi
+}
+
+SCRIPTPATH=$(absPath $(dirname $0))
+
+# SCRIPTPATH=$?
+
+if [[ -s "~/.zshrc" ]] ; then
+    mv ~/.zshrc ~/.zshrc.bac
+fi
 ln -s ${SCRIPTPATH}/../.zshrc ~/.zshrc
